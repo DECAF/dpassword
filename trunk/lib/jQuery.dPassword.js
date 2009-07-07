c25c75aa598420cc709e3cd6e72396f2a478c2e7
su@decaf.de <su@decaf.de@cacf874c-6745-11de-a4cb-4929aab1b576>
2009-07-07 13:57:01 +0000

    license info added

--- trunk/lib/jQuery.dPassword.js
+++ trunk/lib/jQuery.dPassword.js
@@ -1,9 +1,27 @@
 /*******************************************************************************
  * dPassword v0.1 - jQuery delayed password masking (iPhone style)
  *
- * (c) copyright 2009 by DECAF°, Stefan Ullrich (http://decaf.de)
- *     Feel free to redistribute or modify this script, as long
- *     as you leave this copyright disclaimer at the top
+ * licensed under MIT License
+ *
+ * Copyright (c) 2009 DECAF°, Stefan Ullrich (http://decaf.de)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining 
+ * a copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be 
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR 
+ * THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  * Known Issues: - delete marked text will not work
  *               - deleting a single chars will not work if char is 
